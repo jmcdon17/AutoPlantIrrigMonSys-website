a1608937f84f7291fa044f77ec7674669eeb4667
Justin McDonald <jmcdon17@gmail.com>
2024-04-14 23:37:24 -0500

    implemented bar chart

--- dist/assets/demo/chart-area-demo.js
+++ dist/assets/demo/chart-area-demo.js
@@ -6,10 +6,11 @@ Chart.defaults.global.defaultFontColor = '#292b2c';
 d3.csv("datalogs.csv").then(function(data) {
   // Parse the dates and convert the other columns to numbers
   data.forEach(function(datapoints) {
-    datapoints.time = d3.timeParse("%m/%d/%Y %H:%M")(datapoints.time);
+    //datapoints.time = d3.timeParse("%m/%d/%Y %H:%M")(datapoints.time);
+    datapoints.time = datapoints.time;
     datapoints.temperature = +datapoints.temperature;
     datapoints.humidity = +datapoints.humidity;
-    //datapoints.water = +datapoints.water;
+    datapoints.water = +datapoints.water;
     datapoints.soilmoisture = +datapoints.soilmoisture;
   });
 
@@ -17,7 +18,7 @@ d3.csv("datalogs.csv").then(function(data) {
   var labels = data.map(function(datapoints) { return datapoints.time;});
   var tempData = data.map(function(datapoints) { return datapoints.temperature;});
   var humidityData = data.map(function(datapoints) { return datapoints.humidity;});
-  //var waterData = data.map(function(datapoints) { return datapoints.water;});
+  var waterData = data.map(function(datapoints) { return datapoints.water;});
   var soilData = data.map(function(datapoints) { return datapoints.soilmoisture;});
 
   // Create Multi-line Chart
@@ -70,27 +71,42 @@ d3.csv("datalogs.csv").then(function(data) {
         pointBorderWidth: 2,
         data: soilData,
         hidden: false,
+      },
+      {
+        label: "Water Level",
+        lineTension: 0.3,
+        backgroundColor: "rgba(153,102,255,0.2)",
+        borderColor: "rgba(153,102,255,1)",
+        pointRadius: 3,
+        pointBackgroundColor: "rgba(153,102,255,1)",
+        pointBorderColor: "rgba(255,255,255,0.8)",
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgba(153,102,255,1)",
+        pointHitRadius: 5,
+        pointBorderWidth: 2,
+        data: waterData,
+        hidden: true,
       }],
     },
     options: {
       scales: {
         xAxes: [{
           time: {
-            unit: 'date',
+            unit: 'string',
             //parser: 'DD/MM/YYYY HH:mm'
           },
           gridLines: {
             display: false
           },
           ticks: {
-            maxTicksLimit: 7
+            maxTicksLimit: 40
           }
         }],
         yAxes: [{
           ticks: {
             min: 0,
             max: 100,
-            maxTicksLimit: 5
+            maxTicksLimit: 30
           },
           gridLines: {
             color: "rgba(0, 0, 0, .125)",
--- dist/assets/demo/chart-bar-demo.js
+++ dist/assets/demo/chart-bar-demo.js
@@ -2,45 +2,78 @@
 Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#292b2c';
 
+// Load data from CSV file
+d3.csv("datalogs.csv").then(function(data) {
+  // Parse the dates and convert the other columns to numbers
+  data.forEach(function(datapoints) {
+    //datapoints.time = d3.timeParse("%m/%d/%Y %H:%M")(datapoints.time);
+    datapoints.time = datapoints.time;
+    //datapoints.temperature = +datapoints.temperature;
+    //datapoints.humidity = +datapoints.humidity;
+    datapoints.water = +datapoints.water;
+    //datapoints.soilmoisture = +datapoints.soilmoisture;
+  });
+
+  // Create arrays for the labels and each dataset
+  var labels = data.map(function(datapoints) { return datapoints.time;});
+  //var tempData = data.map(function(datapoints) { return datapoints.temperature;});
+  //var humidityData = data.map(function(datapoints) { return datapoints.humidity;});
+  var waterData = data.map(function(datapoints) { return datapoints.water;});
+  //var soilData = data.map(function(datapoints) { return datapoints.soilmoisture;});
+
 // Bar Chart Example
 var ctx = document.getElementById("myBarChart");
 var myLineChart = new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ["January", "February", "March", "April", "May", "June"],
+    labels: labels.slice(-20), // Only take the last 10 labels
     datasets: [{
-      label: "Revenue",
-      backgroundColor: "rgba(2,117,216,1)",
-      borderColor: "rgba(2,117,216,1)",
-      data: [4215, 5312, 6251, 7841, 9821, 14984],
+    label: "Water Level",
+    lineTension: 0.3,
+    backgroundColor: "rgba(2,117,216,0.7)",
+    borderColor: "rgba(2,117,216,1)",
+    pointRadius: 3,
+    pointBackgroundColor: "rgba(2,117,216,1)",
+    pointBorderColor: "rgba(255,255,255,0.8)",
+    pointHoverRadius: 5,
+    pointHoverBackgroundColor: "rgba(2,117,216,1)",
+    pointHitRadius: 5,
+    pointBorderWidth: 2,
+    data: waterData.slice(-20), // Only take the last 10 data points
+    hidden: false,
     }],
   },
   options: {
     scales: {
-      xAxes: [{
-        time: {
-          unit: 'month'
-        },
-        gridLines: {
-          display: false
-        },
-        ticks: {
-          maxTicksLimit: 6
-        }
-      }],
-      yAxes: [{
-        ticks: {
-          min: 0,
-          max: 15000,
-          maxTicksLimit: 5
-        },
-        gridLines: {
-          display: true
-        }
-      }],
+    xAxes: [{
+      time: {
+      unit: 'string',
+      //parser: 'DD/MM/YYYY HH:mm'
+      },
+      gridLines: {
+      display: false
+      },
+      ticks: {
+      maxTicksLimit: 20 // Limit the ticks to 10
+      }
+    }],
+    yAxes: [{
+      ticks: {
+      min: 0,
+      max: 3,
+      maxTicksLimit: 30
+      },
+      gridLines: {
+      color: "rgba(0, 0, 0, .125)",
+      }
+    }],
     },
     legend: {
-      display: false
+    display: true
     }
   }
+  });
+
+}).catch(function(error) {
+  console.log(error);
 });
